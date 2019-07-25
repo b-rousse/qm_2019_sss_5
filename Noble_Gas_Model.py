ae0bbab2fb34e4961f91911a4d0edbfc948cef28
riddhish-P <riddhish.u.p@gmail.com>
2019-07-25 16:07:31 -0500

    NobleGasModel created. A test is written in comments

--- Noble_Gas_Model.py
+++ Noble_Gas_Model.py
@@ -1,5 +1,5 @@
 class Noble_Gas_Model():
-    def __init__(self, model_parameters, ionic_charge,orbital_types,p_orbitals,orbitals_per_atom,vec, orbital_occupations):
+    def __init__(self):
         self.model_parameters = {
          'coulomb_p': -0.010255409806855187,
          'coulomb_s': 0.4536486561938202,
@@ -16,19 +16,18 @@ class Noble_Gas_Model():
                                 }
         self.ionic_charge = 6
         self.orbital_types = ['s', 'px', 'py', 'pz']
-        self.p_orbitals = orbital_types[1:]
+        self.p_orbitals = self.orbital_types[1:]
         self.orbitals_per_atom = len(self.orbital_types)
         self.vec = {'px': [1, 0, 0], 'py': [0, 1, 0], 'pz': [0, 0, 1]}
         self.orbital_occupations = { 's':0, 'px':1, 'py':1, 'pz':1 }
         
 
     def __str__(self):
-        return 'name: ' + self.surname + ', ' +self.name + '\ncourses: ' + str(self.courses)
+        return 'Noble Gas Model created with '+str(self.orbital_types)+' orbitals and an ionic charge of '+ str(self.ionic_charge) + '.'
 
-    def atom(ao_index):
+    def atom(self, ao_index):
         '''Returns the atom index part of an atomic orbital index.
 
-        This is a longer explanation of what the function does, NOT how the function does it. 
 
         Parameters
         ----------
@@ -40,17 +39,50 @@ class Noble_Gas_Model():
         ao_index // orbitals_per_atom
             An integer which is the index of the atom that the atomic orbital is centered on.
         '''
-        return ao_index // orbitals_per_atom
+        return ao_index // self.orbitals_per_atom
 
-    def orb(ao_index):
+    def orb(self, ao_index):
         '''Returns the orbital type of an atomic orbital index.
         
-        This is a more detailed description'''
-        orb_index = ao_index % orbitals_per_atom
-        return orbital_types[orb_index]
+        Parameters
+        ----------
+        ao_index : int
+            index of atomic orbital.
 
-    def ao_index(atom_p, orb_p):
-        '''Returns the atomic orbital index for a given atom index and orbital type.'''
-        p = atom_p * orbitals_per_atom
-        p += orbital_types.index(orb_p)
+        Returns
+        -------
+        ao_index % orbitals_per_atom
+            A string which is the atomic orbital.
+        '''
+        orb_index = ao_index % self.orbitals_per_atom
+        return self.orbital_types[orb_index]
+
+    def ao_index(self, atom_p, orb_p):
+        '''
+        Returns the atomic orbital index for a given atom index and orbital type.
+        
+        Parameters
+        ----------
+        atom_p : int
+            index of the atom.
+        orb_p :  str
+            type of the orbital
+        Returns
+        -------
+        (atom_p * self.orbitals_per_atom ) + self.orbital_types.index(orb_p)
+            The complete index of the atomic orbital.
+        '''
+        p = atom_p * self.orbitals_per_atom
+        p += self.orbital_types.index(orb_p)
         return p
+
+
+"""
+Test to be put later of=n...
+import numpy as np
+atomic_coordinates = np.array([[0.0, 0.0, 0.0], [3.0, 4.0, 5.0]])
+number_of_atoms = len(atomic_coordinates)
+nbg1 = Noble_Gas_Model()
+for index in range(number_of_atoms * nbg1.orbitals_per_atom):
+    assert (index == nbg1.ao_index(nbg1.atom(index), nbg1.orb(index)))
+"""
