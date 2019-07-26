fa09ee348c225ef9ee6bf2cd937aef41c7b04bb6
Ben Rousseau <b.g.rousseau@gmail.com>
2019-07-26 16:34:47 -0500

    Let's make some unit teeeests

--- routine.py
+++ routine.py
@@ -12,4 +12,5 @@ if __name__ == "__main__":
     energy_scf = hartree_fock_instance.calculate_energy_scf()
     energy_ion = hartree_fock_instance.calculate_energy_ion(NobleGasModel)
     print(F'The SCF energy is  {energy_scf} and the ion energy is {energy_ion} ')
-    
+    #mp2_instance = mp2.MP2(hartree_fock_instance)
+    #print(F'The MP2 energy is {mp2.MP2.calculate_energy_mp2}')
