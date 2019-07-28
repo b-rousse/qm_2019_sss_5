322b683f0339ee228e35f12b0e3076129919c659
Ben Rousseau <b.g.rousseau@gmail.com>
2019-07-28 11:35:19 -0500

    bringing mp2 into the mix

--- mp2.py
+++ mp2.py
@@ -1,17 +1,17 @@
 import numpy as np
 from hartree_fock import HartreeFock
 class MP2(HartreeFock):
-    def __init__(self, fock_matrix, interaction_matrix, chi_tensor, NobleGasModel):
-        super().__init__(NobleGasModel)
+    def __init__(self, NobleGasModel, atomic_coordinates):
+        super().__init__(NobleGasModel, atomic_coordinates)
         self.orbitals_per_atom = NobleGasModel.orbitals_per_atom
         self.ionic_charge = NobleGasModel.ionic_charge
         self.energy_mp2 = 0.0
 
 
-    def partition_orbitals(self, fock_matrix):
+    def partition_orbitals(self):
         '''Returns a list with the occupied/virtual energies & orbitals defined by the input Fock matrix.'''
-        num_occ = (self.ionic_charge // 2) * np.size(fock_matrix,0) // self.orbitals_per_atom
-        orbital_energy, orbital_matrix = np.linalg.eigh(fock_matrix)
+        num_occ = (self.ionic_charge // 2) * np.size(self.fock_matrix,0) // self.orbitals_per_atom
+        orbital_energy, orbital_matrix = np.linalg.eigh(self.fock_matrix)
         occupied_energy = orbital_energy[:num_occ]
         virtual_energy = orbital_energy[num_occ:]
         occupied_matrix = orbital_matrix[:, :num_occ]
@@ -19,16 +19,16 @@ class MP2(HartreeFock):
 
         return occupied_energy, virtual_energy, occupied_matrix, virtual_matrix
 
-    def transform_interaction_tensor(self, occupied_matrix, virtual_matrix, interaction_matrix, chi_tensor):
+    def transform_interaction_tensor(self, occupied_matrix, virtual_matrix):
         '''Returns a transformed V tensor defined by the input occupied, virtual, & interaction matrices.'''
-        chi2_tensor = np.einsum('qa,ri,qrp', virtual_matrix, occupied_matrix, chi_tensor, optimize=True)
-        interaction_tensor = np.einsum('aip,pq,bjq->aibj', chi2_tensor, interaction_matrix, chi2_tensor, optimize=True)
+        chi2_tensor = np.einsum('qa,ri,qrp', virtual_matrix, occupied_matrix, self.chi_tensor, optimize=True)
+        interaction_tensor = np.einsum('aip,pq,bjq->aibj', chi2_tensor, self.interaction_matrix, chi2_tensor, optimize=True)
         return interaction_tensor
 
-    def calculate_energy_mp2(self, fock_matrix, interaction_matrix, chi_tensor):
+    def calculate_energy_mp2(self):
         '''Returns the MP2 contribution to the total energy defined by the input Fock & interaction matrices.'''
-        E_occ, E_virt, occupied_matrix, virtual_matrix = self.partition_orbitals(fock_matrix)
-        V_tilde = self.transform_interaction_tensor(occupied_matrix, virtual_matrix, interaction_matrix, chi_tensor)
+        E_occ, E_virt, occupied_matrix, virtual_matrix = self.partition_orbitals()
+        V_tilde = self.transform_interaction_tensor(occupied_matrix, virtual_matrix)
 
         energy_mp2 = 0.0
         num_occ = len(E_occ)
